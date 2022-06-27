bcfc59f92e55279a569d5178d9ed88ac6ef82b5b
Tim Araque <timaraque@hotmail.com>
2022-06-26 21:01:34 -0400

    branch frontend creating screen of final results and creating method to obtain the results of the model
    Signed-off-by: Tim Araque <timaraque@hotmail.com>
    
     On branch frontend
     Changes to be committed:
    	modified:   application/controllers/Survey.php
    	new file:   application/views/survey_results.php

--- application/controllers/Survey.php
+++ application/controllers/Survey.php
@@ -3,8 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Survey extends CI_Controller {
 
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('survey_m');
+    }
+
     public function index(){
 
+        $data['count_surveys_completed'] = $this-survey_m->get_count_surveys_completed();
         $data['subview'] = 'index';
         $this->load->view('_survey_layout', $data);
     }
---
+++ application/views/survey_results.php
@@ -0,0 +1,7 @@
+<h2> Resultados de la Encuesta: </h2>
+<ul>
+    <li><strong>Encuestas completadas</strong> <?php echo $count_surveys_completed; ?> </li>
+    <li><strong>Tipo:</strong> Varias preguntas</li>
+    <li><strong>Tiempo estimado:</strong> 1 Minuto </li>
+</ul>
+<a href="<?php echo site_url('survey/question'); ?>" class="btn btn-outline-primary">Empezar de nuevo</a>
