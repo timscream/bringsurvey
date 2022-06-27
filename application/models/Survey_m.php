cf4e72355a0ef00c26e92ba7d70def3b57fade27
Tim Araque <timaraque@hotmail.com>
2022-06-26 22:59:54 -0400

    branch backend creating method avg for social network
    Signed-off-by: Tim Araque <timaraque@hotmail.com>
    
     On branch backend
     Changes to be committed:
    	modified:   application/models/Survey_m.php

--- application/models/Survey_m.php
+++ application/models/Survey_m.php
@@ -8,10 +8,10 @@ class Survey_m extends CI_Model {
         return $this->db->count_all('surveys_completed');
     }
 
-    public function get_question($question_id){
+    public function get_avg_social_network($question_id){
 
-        $this->db->where('question_id', $question_id);
-        return $this->db->get('users')->row();
+        $this->db->select('AVG(avg_fb) avg_fb, AVG(avg_wa) avg_wa, AVG(avg_tw) avg_tw, AVG(avg_ig) avg_ig, AVG(avg_tt) avg_tt');
+        return $this->db->get('surveys_completed')->row();
     }
 }
 
