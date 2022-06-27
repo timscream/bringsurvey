99b1ca948cbfa9f39f591c7d2a16163b48672474
Tim Araque <timaraque@hotmail.com>
2022-06-26 22:25:12 -0400

    branch backend update config database.php, creating method for get count surveys
    Signed-off-by: Tim Araque <timaraque@hotmail.com>
    
     On branch backend
     Changes to be committed:
    	modified:   application/config/database.php
    	modified:   application/models/Survey_m.php

--- application/models/Survey_m.php
+++ application/models/Survey_m.php
@@ -3,9 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Survey_m extends CI_Model {
 
-    public function get_count_surveys(){
+    public function get_count_surveys_completed(){
 
         return $this->db->count_all('surveys_completed');
+    }
+
+    public function get_question($question_id){
+
+        $this->db->where('question_id', $question_id);
+        return $this->db->get('users')->row();
     }
 }
 
