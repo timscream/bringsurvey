6ac654b10c6c415531d13b4bcbfa420d66fa3f9a
Tim Araque <timaraque@hotmail.com>
2022-06-27 02:40:39 -0400

    branch backend correcting user in database.php, including methods get_favorite_social_network and get_less_dear_social_network
    Signed-off-by: Tim Araque <timaraque@hotmail.com>
    
     On branch backend
     Changes to be committed:
    	modified:   application/config/database.php
    	modified:   application/models/Survey_m.php

--- application/models/Survey_m.php
+++ application/models/Survey_m.php
@@ -8,12 +8,32 @@ class Survey_m extends CI_Model {
         return $this->db->count_all('surveys_completed');
     }
 
-    public function get_avg_social_network($question_id){
+    public function get_avg_social_network(){
 
         $this->db->select('AVG(avg_fb) avg_fb, AVG(avg_wa) avg_wa, AVG(avg_tw) avg_tw, AVG(avg_ig) avg_ig, AVG(avg_tt) avg_tt');
         return $this->db->get('surveys_completed')->row();
     }
+
+    public function get_favorite_social_network(){
+
+        $this->db->select('`favorite_social_network`, count(favorite_social_network) as total');
+        $this->db->from('surveys_completed');
+        $this->db->group_by('favorite_social_network');
+        $this->db->order_by('total', 'DESC');
+        $this->db->limit(1);
+        return $this->db->get()->row();
+    }
+
+    public function get_less_dear_social_network(){
+
+        $this->db->select('`favorite_social_network` as less_dear_social_network, count(favorite_social_network) as total');
+        $this->db->from('surveys_completed');
+        $this->db->group_by('favorite_social_network');
+        $this->db->order_by('total', 'ASC');
+        $this->db->limit(1);
+        return $this->db->get()->row();
+    }
 }
 
-/* End of file Survey_m.php */
+/* End of file survey_m.php */
 /* Location: ./application/models/Survey_m.php */
